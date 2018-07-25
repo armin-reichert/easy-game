4acce3699d6fa016249b8163e2e057630cb3d772
Armin Reichert <armin.reichert@web.de>
2018-07-25 11:28:29 +0200

    improved

--- EasyGame/src/de/amr/easy/game/sprite/LinearAnimation.java
+++ EasyGame/src/de/amr/easy/game/sprite/LinearAnimation.java
@@ -3,15 +3,14 @@ package de.amr.easy.game.sprite;
 import java.awt.Image;
 
 public class LinearAnimation extends Animation {
-	
+
 	public LinearAnimation(Image... frames) {
 		super(frames);
 	}
-	
+
 	@Override
 	protected void nextFrame() {
 		if (frameIndex < frames.length - 1)
 			++frameIndex;
 	}
-
 }
