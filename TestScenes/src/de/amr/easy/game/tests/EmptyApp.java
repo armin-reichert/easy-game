b8ab6deb13f059be5bc23cddb6b9e2e690a2f0fa
Armin Reichert <armin.reichert@web.de>
2018-09-05 17:34:39 +0200

    test app

---
+++ TestScenes/src/de/amr/easy/game/tests/EmptyApp.java
@@ -0,0 +1,16 @@
+package de.amr.easy.game.tests;
+
+import de.amr.easy.game.Application;
+
+
+public class EmptyApp extends Application {
+
+	@Override
+	public void init() {
+	}
+
+	public static void main(String[] args) {
+		launch(new EmptyApp());
+	}
+
+}
