4725c8f687f60f1dba1a64ec38a5a4417a1ee7da
Armin Reichert <armin.reichert@web.de>
2018-12-26 14:56:17 +0100

    doc

--- EasyGame/src/de/amr/easy/game/math/Vector2f.java
+++ EasyGame/src/de/amr/easy/game/math/Vector2f.java
@@ -4,49 +4,147 @@ import static java.lang.Math.acos;
 import static java.lang.Math.toDegrees;
 
 /**
- * 2D vector with float precision.
+ * Immutable 2D vector with float precision.
  * 
  * @author Armin Reichert
  */
 public class Vector2f {
 
+	/** The x-coordinate. */
+	public final float x;
+
+	/** The y-coordinate. */
+	public final float y;
+
+	/**
+	 * The NULL vector.
+	 */
 	public static final Vector2f NULL = Vector2f.of(0, 0);
 
+	/**
+	 * Creates a vector with given x and y coordinates.
+	 * 
+	 * @param x
+	 *            the x-coordinate
+	 * @param y
+	 *            the y-coordinate
+	 * @return vector with given coordinates
+	 */
 	public static Vector2f of(float x, float y) {
-		return new Vector2f(x, y);
+		return x == 0 && y == 0 ? NULL : new Vector2f(x, y);
 	}
 
+	/**
+	 * 
+	 * @param v
+	 *            first vector
+	 * @param w
+	 *            second vector
+	 * @return sum of the two vectors
+	 */
 	public static Vector2f sum(Vector2f v, Vector2f w) {
 		return Vector2f.of(v.x + w.x, v.y + w.y);
 	}
 
+	/**
+	 * 
+	 * @param v
+	 *            a vector
+	 * @return the inverse vector
+	 */
 	public static Vector2f inverse(Vector2f v) {
 		return Vector2f.of(-v.x, -v.y);
 	}
 
+	/**
+	 * 
+	 * @param v
+	 *            first vector
+	 * @param w
+	 *            second vector
+	 * @return the difference vector <code>v - w</code>
+	 */
 	public static Vector2f diff(Vector2f v, Vector2f w) {
 		return Vector2f.of(v.x - w.x, v.y - w.y);
 	}
 
+	/**
+	 * 
+	 * @param f
+	 *            a scalar value
+	 * @param v
+	 *            a vector
+	 * @return the vector given by multiplying each coordinate with the scalar value
+	 */
 	public static Vector2f smul(float f, Vector2f v) {
 		return Vector2f.of(f * v.x, f * v.y);
 	}
 
+	/**
+	 * 
+	 * @param v
+	 *            first vector
+	 * @param w
+	 *            second vector
+	 * @return the scalar product of both vectors
+	 */
 	public static float dot(Vector2f v, Vector2f w) {
 		return v.x * w.x + v.y * w.y;
 	}
 
+	/**
+	 * 
+	 * @param v
+	 *            first vector
+	 * @param w
+	 *            second vector
+	 * @return the Euclidean distance between the points defined by the vectors
+	 */
 	public static float dist(Vector2f v, Vector2f w) {
 		return diff(v, w).length();
 	}
 
+	/**
+	 * 
+	 * @param v
+	 *            first vector
+	 * @param w
+	 *            second vector
+	 * @return the angle between the vectors (in degrees)
+	 */
 	public static double angle(Vector2f v, Vector2f w) {
 		double cos_phi = dot(v, w) / (v.length() * w.length());
 		return toDegrees(acos(cos_phi));
 	}
 
-	public final float x;
-	public final float y;
+	/**
+	 * @return x-coordinate rounded to the closest integer
+	 */
+	public int roundedX() {
+		return Math.round(x);
+	}
+
+	/**
+	 * @return y-coordinate rounded to the closest integer
+	 */
+	public int roundedY() {
+		return Math.round(y);
+	}
+
+	/**
+	 * @return The normalized vector, that is the unit vector with the same direction.
+	 */
+	public Vector2f normalized() {
+		float len = length();
+		return Vector2f.of(x / len, y / len);
+	}
+
+	/**
+	 * @return The length of the vector.
+	 */
+	public float length() {
+		return (float) Math.hypot(x, y);
+	}
 
 	private Vector2f(float x, float y) {
 		this.x = x;
@@ -77,20 +175,4 @@ public class Vector2f {
 		return "(" + x + "," + y + ")";
 	}
 
-	public int roundedX() {
-		return Math.round(x);
-	}
-
-	public int roundedY() {
-		return Math.round(y);
-	}
-
-	public Vector2f normalized() {
-		float len = length();
-		return Vector2f.of(x / len, y / len);
-	}
-
-	public float length() {
-		return (float) Math.hypot(x, y);
-	}
 }
