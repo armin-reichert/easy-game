48b7e057c6a110f3901a290232ed15a88a435b9e
Armin Reichert <armin.reichert@web.de>
2020-04-28 16:25:39 +0200

    convenience class

---
+++ EasyGame/src/main/java/de/amr/easy/game/controller/GameObject.java
@@ -0,0 +1,13 @@
+package de.amr.easy.game.controller;
+
+import de.amr.easy.game.entity.Entity;
+import de.amr.easy.game.view.View;
+
+/**
+ * A "all-in-one" game object.
+ * 
+ * @author Armin Reichert
+ */
+public abstract class GameObject extends Entity implements Lifecycle, View {
+
+}
