d3c0bf558dabff428b41c1ff54e955390ae02d9e
Armin Reichert <armin.reichert@web.de>
2020-05-24 14:08:16 +0200

    new method

--- EasyGame/src/main/java/de/amr/easy/game/entity/EntityMap.java
+++ EasyGame/src/main/java/de/amr/easy/game/entity/EntityMap.java
@@ -5,12 +5,18 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
+/**
+ * An entity map that allows to store entities with a unique key or anonymously and that provides a
+ * number of useful methods for accessing entities.
+ * 
+ * @author Armin Reichert
+ */
 public class EntityMap {
 
-	private final Map<String, Entity> map = new ConcurrentHashMap<>();
+	private final Map<String, Entity> entries = new ConcurrentHashMap<>();
 
 	public <E extends Entity> E store(String key, E entity) {
-		map.put(key, entity);
+		entries.put(key, entity);
 		return entity;
 	}
 
@@ -19,36 +25,40 @@ public class EntityMap {
 	}
 
 	public void remove(String key) {
-		map.remove(key);
+		entries.remove(key);
 	}
 
 	public void removeEntity(Entity entity) {
-		map.entrySet().removeIf(entry -> entry.getValue().equals(entity));
+		entries.entrySet().removeIf(e -> e.getValue().equals(entity));
 	}
 
-	public void removeAll(Class<? extends Entity> classToDelete) {
-		map.entrySet().removeIf(entry -> classToDelete.isAssignableFrom(entry.getValue().getClass()));
+	public void removeAll(Class<? extends Entity> class_) {
+		entries.entrySet().removeIf(e -> class_.isAssignableFrom(e.getValue().getClass()));
 	}
 
 	public boolean contains(String key) {
-		return map.containsKey(key);
+		return entries.containsKey(key);
 	}
 
 	public Stream<Entity> all() {
-		return map.values().stream();
+		return entries.values().stream();
 	}
 
 	public Stream<Entity> filter(Predicate<? super Entity> predicate) {
-		return map.values().stream().filter(predicate);
+		return entries.values().stream().filter(predicate);
 	}
 
 	@SuppressWarnings("unchecked")
 	public <E extends Entity> E named(String name) {
-		return (E) map.get(name);
+		return (E) entries.get(name);
 	}
 
-	@SuppressWarnings("unchecked")
-	public <E> Stream<E> ofClass(Class<E> cls) {
-		return map.values().stream().filter(e -> e.getClass().equals(cls)).map(e -> (E) e);
+	public <E extends Entity> Stream<E> ofClass(Class<E> class_) {
+		return filter(e -> e.getClass().equals(class_)).map(class_::cast);
 	}
+
+	public <E> Stream<E> implementing(Class<E> interface_) {
+		return filter(e -> interface_.isAssignableFrom(e.getClass())).map(interface_::cast);
+	}
+
 }
