4b99d90d49df6b8ba2663e2bf7250e33f55d4b93
Armin Reichert <armin.reichert@web.de>
2020-05-25 06:43:00 +0200

    default impl

--- EasyGame/src/main/java/de/amr/easy/game/controller/GameObject.java
+++ EasyGame/src/main/java/de/amr/easy/game/controller/GameObject.java
@@ -1,13 +1,29 @@
 package de.amr.easy.game.controller;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+
 import de.amr.easy.game.entity.Entity;
 import de.amr.easy.game.view.View;
 
 /**
- * A "all-in-one" game object.
+ * A "all-in-one" (entity, view, life-cycle) game object.
  * 
  * @author Armin Reichert
  */
 public abstract class GameObject extends Entity implements Lifecycle, View {
 
+	@Override
+	public void init() {
+	};
+
+	@Override
+	public void update() {
+	}
+
+	@Override
+	public void draw(Graphics2D g) {
+		g.setColor(Color.RED);
+		g.drawRect((int) tf.x, (int) tf.y, tf.width, tf.height);
+	}
 }
