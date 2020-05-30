84f23da374f33aa8c28b837de7e192dded8d1b3f
Armin Reichert <armin.reichert@web.de>
2020-05-30 17:08:19 +0200

    cleanup

--- EasyGame/src/main/java/de/amr/easy/game/timing/Clock.java
+++ EasyGame/src/main/java/de/amr/easy/game/timing/Clock.java
@@ -1,35 +1,30 @@
 package de.amr.easy.game.timing;
 
-import static java.lang.String.format;
+import static de.amr.easy.game.Application.loginfo;
 import static java.util.concurrent.TimeUnit.NANOSECONDS;
 import static java.util.concurrent.TimeUnit.SECONDS;
 
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
- * The clock that drives the game loop.
+ * The clock that drives the application.
  * 
  * @author Armin Reichert
  */
 public class Clock {
 
-	private static final Logger LOGGER = Logger.getLogger(Clock.class.getName());
-
-	private volatile boolean running;
+	public volatile boolean logging = false;
+	private volatile boolean ticking;
 	private Runnable work;
 	private Thread thread;
 	private int targetFPS;
 	private long period;
 	private long totalTicks;
-
 	private int frames;
-	private int fps;
-	private long fpsMmtStart = 0;
-
-	private PropertyChangeSupport pcs = new PropertyChangeSupport(this);
+	private int framesPerSec;
+	private long frameRateMeasurementStart = 0;
+	private PropertyChangeSupport changes = new PropertyChangeSupport(this);
 
 	/**
 	 * Creates a clock which triggers execution of the given work according to the clock frequency.
@@ -40,14 +35,85 @@ public class Clock {
 	public Clock(int targetFrequency, Runnable work) {
 		setTargetFramerate(targetFrequency);
 		this.work = work;
-		LOGGER.setLevel(Level.OFF);
+	}
+
+	/**
+	 * Starts the clock and the thread.
+	 */
+	public synchronized void start() {
+		if (!ticking) {
+			totalTicks = 0;
+			ticking = true;
+			thread = new Thread(this::run, "Clock");
+			thread.start();
+		}
+	}
+
+	/**
+	 * Stops the clock and the thread.
+	 */
+	public synchronized void stop() {
+		if (ticking) {
+			ticking = false;
+			try {
+				thread.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private void run() {
+		while (ticking) {
+			tick();
+		}
+	}
+
+	private void tick() {
+		long start = System.nanoTime();
+		work.run();
+		long frameDuration = System.nanoTime() - start;
+		if (logging) {
+			loginfo("%-15s: %15.2f ms", "Frame", frameDuration / 1_000_000f);
+		}
+		++totalTicks;
+
+		// measure FPS
+		++frames;
+		if (System.nanoTime() >= frameRateMeasurementStart + SECONDS.toNanos(1)) {
+			framesPerSec = frames;
+			frames = 0;
+			frameRateMeasurementStart = System.nanoTime();
+		}
+
+		// sleep as long as needed to reach target FPS
+		long timeLeft = (period - frameDuration);
+		if (timeLeft > 0) {
+			try {
+				NANOSECONDS.sleep(timeLeft * 975 / 1000); // give a little reserve time
+				if (logging) {
+					loginfo("%-15s: %15.2f ms", "Slept", timeLeft / 1_000_000f);
+				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * Adds a listener for frequency changes.
+	 * 
+	 * @param listener frequency change listener
+	 */
+	public void addFrequencyChangeListener(PropertyChangeListener listener) {
+		changes.addPropertyChangeListener("frequency", listener);
 	}
 
 	/**
 	 * @return last reported number of frames/second
 	 */
 	public int getFrameRate() {
-		return fps;
+		return framesPerSec;
 	}
 
 	/**
@@ -63,96 +129,34 @@ public class Clock {
 	 * @param fps number of ticks per second
 	 */
 	public void setTargetFramerate(int fps) {
+		if (this.targetFPS == fps) {
+			return;
+		}
 		if (fps < 1) {
 			throw new IllegalArgumentException("Clock frequency must be at least 1");
 		}
-		int oldTargetFPS = this.targetFPS;
-		this.targetFPS = fps;
+		int oldTargetFPS = targetFPS;
+		targetFPS = fps;
 		period = SECONDS.toNanos(1) / fps;
-		if (oldTargetFPS != targetFPS) {
-			pcs.firePropertyChange("frequency", oldTargetFPS, targetFPS);
-			LOGGER.info(String.format("Clock frequency changed to %d ticks/sec.", targetFPS));
-		}
+		loginfo(String.format("Clock frequency changed to %d ticks/sec.", targetFPS));
+		changes.firePropertyChange("frequency", oldTargetFPS, targetFPS);
 	}
 
 	/**
-	 * @return number of ticks since the clock was started
+	 * @return total number of ticks since the clock was started
 	 */
 	public long getTotalTicks() {
 		return totalTicks;
 	}
 
 	/**
+	 * Converts a given time (in seconds) into the number of corresponding ticks at the clock's target
+	 * speed.
+	 * 
 	 * @param seconds seconds
 	 * @return number of clock ticks representing the given seconds
 	 */
 	public int sec(float seconds) {
 		return Math.round(targetFPS * seconds);
-	}
-
-	/**
-	 * Adds a listener for frequency changes.
-	 * 
-	 * @param listener frequency change listener
-	 */
-	public void addFrequencyChangeListener(PropertyChangeListener listener) {
-		pcs.addPropertyChangeListener("frequency", listener);
-	}
-
-	/**
-	 * Starts the clock and the thread on which the work is executed.
-	 */
-	public synchronized void start() {
-		if (!running) {
-			totalTicks = 0;
-			running = true;
-			thread = new Thread(this::loop, "Clock");
-			thread.start();
-		}
-	}
-
-	/**
-	 * Stops the clock and the game loop thread.
-	 */
-	public synchronized void stop() {
-		if (running) {
-			running = false;
-			try {
-				thread.join();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-
-	private void loop() {
-		while (running) {
-
-			long start = System.nanoTime();
-			work.run();
-			long frameDuration = System.nanoTime() - start;
-			LOGGER.info(() -> format("%-15s: %15.2f ms", "Frame", frameDuration / 1_000_000f));
-
-			++totalTicks;
-
-			// measure FPS
-			++frames;
-			if (System.nanoTime() >= fpsMmtStart + SECONDS.toNanos(1)) {
-				fps = frames;
-				frames = 0;
-				fpsMmtStart = System.nanoTime();
-			}
-
-			// sleep as long as needed to reach target FPS
-			long timeLeft = (period - frameDuration);
-			if (timeLeft > 0) {
-				try {
-					NANOSECONDS.sleep(timeLeft * 975 / 1000);
-					LOGGER.info(() -> format("%-15s: %15.2f ms", "Slept", timeLeft / 1_000_000f));
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-		}
 	}
 }
