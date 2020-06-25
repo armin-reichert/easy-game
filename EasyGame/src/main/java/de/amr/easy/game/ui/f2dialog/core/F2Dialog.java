5592c832a969ef488c2df63f9c0f77f525da82c7
Armin Reichert <armin.reichert@web.de>
2020-06-25 16:36:23 +0200

    improved

--- EasyGame/src/main/java/de/amr/easy/game/ui/f2dialog/core/F2Dialog.java
+++ EasyGame/src/main/java/de/amr/easy/game/ui/f2dialog/core/F2Dialog.java
@@ -4,6 +4,7 @@ import static de.amr.easy.game.Application.app;
 
 import java.awt.Component;
 import java.awt.Font;
+import java.awt.Image;
 import java.awt.Window;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
@@ -151,8 +152,10 @@ public class F2Dialog extends JDialog implements Lifecycle, F2DialogAPI {
 
 	private void loadIcons() throws IOException {
 		BufferedImage sheet = ImageIO.read(getClass().getResource("/icons/pause-play-and-stop-blank-icons.png"));
-		pauseIcon = new ImageIcon(sheet.getSubimage(0, 0, 155, 155).getScaledInstance(32, 32, BufferedImage.SCALE_SMOOTH));
-		playIcon = new ImageIcon(sheet.getSubimage(198, 0, 155, 155).getScaledInstance(32, 32, BufferedImage.SCALE_SMOOTH));
+		Image pauseImage = sheet.getSubimage(0, 0, 155, 155).getScaledInstance(32, 32, BufferedImage.SCALE_SMOOTH);
+		Image playImage = sheet.getSubimage(198, 0, 155, 155).getScaledInstance(32, 32, BufferedImage.SCALE_SMOOTH);
+		pauseIcon = new ImageIcon(pauseImage);
+		playIcon = new ImageIcon(playImage);
 	}
 
 	private void updatePlayPauseButton() {
