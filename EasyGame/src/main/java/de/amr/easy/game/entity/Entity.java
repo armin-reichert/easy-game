381337608bf599e01b81a7782666fbc408a9def5
Armin Reichert <armin.reichert@web.de>
2020-07-11 11:03:06 +0200

    no need to be abstract

--- EasyGame/src/main/java/de/amr/easy/game/entity/Entity.java
+++ EasyGame/src/main/java/de/amr/easy/game/entity/Entity.java
@@ -7,21 +7,20 @@ import de.amr.easy.game.entity.collision.Collider;
 /**
  * Base class for (game) entities.
  * <p>
- * An entity provides a {@link Transform transform} object that stores the
- * position, velocity and rotation of the object. Entities are also sensitive to
- * collisions. By default, the transform's position denotes the left upper
- * corner of the collision box. Invisible entities do not trigger collisions.
+ * An entity provides a {@link Transform transform} object that stores the position, velocity and
+ * rotation of the object. Entities are also sensitive to collisions. By default, the transform's
+ * position denotes the left upper corner of the collision box. Invisible entities do not trigger
+ * collisions.
  * 
  * @author Armin Reichert
  */
-public abstract class Entity implements Collider {
+public class Entity implements Collider {
 
 	/** The transform for this entity. */
 	public final Transform tf = new Transform();
 
 	/**
-	 * Visibility of this entity. Invisible entities are not rendered and do not
-	 * cause collisions.
+	 * Visibility of this entity. Invisible entities are not rendered and do not cause collisions.
 	 */
 	public boolean visible = true;
 
