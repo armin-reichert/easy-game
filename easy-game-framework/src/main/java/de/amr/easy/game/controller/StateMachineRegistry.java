eb47444da1af259c10a79db2d4ee60c56c9c6452
Armin Reichert <armin.reichert@web.de>
2020-08-25 15:01:16 +0200

    categories

--- easy-game-framework/src/main/java/de/amr/easy/game/controller/StateMachineRegistry.java
+++ easy-game-framework/src/main/java/de/amr/easy/game/controller/StateMachineRegistry.java
@@ -3,11 +3,13 @@ package de.amr.easy.game.controller;
 import static de.amr.easy.game.Application.app;
 import static de.amr.easy.game.Application.loginfo;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import de.amr.statemachine.core.StateMachine;
@@ -21,33 +23,51 @@ public final class StateMachineRegistry {
 
 	public static final StateMachineRegistry REGISTRY = new StateMachineRegistry();
 
-	private final Set<StateMachine<?, ?>> machines;
+	// maps categories to machines
+	private final Map<String, List<StateMachine<?, ?>>> machines;
 
 	private StateMachineRegistry() {
-		machines = new HashSet<>();
+		machines = new HashMap<>();
 	}
 
-	public Collection<StateMachine<?, ?>> machines() {
-		return Collections.unmodifiableSet(machines);
+	public Set<StateMachine<?, ?>> machines() {
+		return machines.values().stream().flatMap(List::stream).collect(Collectors.toSet());
 	}
 
-	public <FSM extends StateMachine<?, ?>> void register(FSM fsm) {
-		machines.add(fsm);
+	public Set<StateMachine<?, ?>> machines(String category) {
+		return machines.entrySet().stream().filter(e -> e.getKey().equals(category)).map(e -> e.getValue())
+				.flatMap(List::stream).collect(Collectors.toSet());
+	}
+
+	public void register(String categoryName, StateMachine<?, ?> fsm) {
+		List<StateMachine<?, ?>> category = machines.get(categoryName);
+		if (category == null) {
+			category = new ArrayList<StateMachine<?, ?>>();
+			machines.put(categoryName, category);
+		}
+		category.add(fsm);
 		fsm.getTracer().setLogger(app().getLogger());
 		loginfo("State machine registered: %s", fsm);
 	}
 
-	public <FSM extends StateMachine<?, ?>> void unregister(FSM fsm) {
-		machines.remove(fsm);
-		fsm.getTracer().setLogger(null);
-		loginfo("State machine unregistered: %s", fsm);
+	public void register(String categoryName, Stream<StateMachine<?, ?>> machines) {
+		machines.forEach(fsm -> register(categoryName, fsm));
 	}
 
-	public <FSM extends StateMachine<?, ?>> void register(Stream<FSM> machines) {
-		machines.filter(Objects::nonNull).forEach(this::register);
+	public void unregister(StateMachine<?, ?> fsm) {
+		Iterator<Map.Entry<String, List<StateMachine<?, ?>>>> it = machines.entrySet().iterator();
+		while (it.hasNext()) {
+			List<StateMachine<?, ?>> category = it.next().getValue();
+			if (category.contains(fsm)) {
+				it.remove();
+				fsm.getTracer().setLogger(null);
+				loginfo("State machine unregistered: %s", fsm);
+				break;
+			}
+		}
 	}
 
 	public <FSM extends StateMachine<?, ?>> void unregister(Stream<FSM> machines) {
-		machines.filter(Objects::nonNull).forEach(this::unregister);
+		machines.forEach(this::unregister);
 	}
 }
